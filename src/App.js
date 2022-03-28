b558c91dac69776c5aad7f2cb8f84fa9e044f4d1
Jazen <jazencode@gmail.com>
2022-03-28 22:10:45 +0800

    Profile finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Home } from './Pages/Home/Home'
 import { Header } from './components/Header/Header'
 import { Nav } from './components/Nav/Nav'
+import { About } from './Pages/About/About';
 
 function App() {
   return (
       <div className="container">
         <div className="top"></div>
         <Header/>
-        <Nav/>
         <Home/>
+        <About/>
       </div>
   );
 }
